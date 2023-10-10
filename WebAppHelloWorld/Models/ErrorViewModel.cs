994ecf59b758ed16271b7bcdbbb1f100a894ec02
Aidan Killeen <aidan.killeen@gmail.com>
2023-10-10 16:07:46 +0100

    end of day 2 - web app hello world

---
+++ WebAppHelloWorld/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppHelloWorld.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
